01f3fca72545fb69c6935576bd48f718cf00b15c
Sy <2548446071@qq.com>
2022-10-19 10:56:03 +0800

    redis基本配置

--- src/main/java/com/tcl/reggie/controller/OrdersController.java
+++ src/main/java/com/tcl/reggie/controller/OrdersController.java
@@ -1,70 +1,1 @@
-package com.tcl.reggie.controller;
-
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.tcl.reggie.common.BaseContext;
-import com.tcl.reggie.common.R;
-import com.tcl.reggie.entity.Dish;
-import com.tcl.reggie.entity.OrderDetail;
-import com.tcl.reggie.entity.Orders;
-import com.tcl.reggie.service.OrderDetailService;
-import com.tcl.reggie.service.OrdersService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/order")
-@Slf4j
-public class OrdersController {
-
-    @Autowired
-    private OrdersService orderService;
-
-    @Autowired
-    private OrderDetailService orderDetailService;
-
-    @PostMapping("/submit")
-    public R<String> submit(@RequestBody Orders orders){
-        orderService.submit(orders);
-        return R.success("下单成功");
-    }
-
-    /**
-     * 查询订单详情（手机端），未实现，好像没有页面
-     * @param page
-     * @param pageSize
-     * @return
-     */
-    @GetMapping("/userPage")
-    public R<Page> userPage(int page,int pageSize){
-        Page<Orders> pageInfo = new Page<>(page, pageSize);
-        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Orders::getId, BaseContext.getCurrentId());
-        queryWrapper.orderByDesc(Orders::getOrderTime);
-
-        orderService.page(pageInfo, queryWrapper);
-
-        return R.success(pageInfo);
-    }
-
-    /**
-     * 未实现，没有实体类
-     * @param page
-     * @param pageSize
-     * @return
-     */
-    @GetMapping("/page")
-    public R<Page> page(int page, int pageSize){
-        Page<Orders> pageInfo = new Page<>(page, pageSize);
-        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Orders::getId, BaseContext.getCurrentId());
-        queryWrapper.orderByDesc(Orders::getOrderTime);
-
-        orderService.page(pageInfo, queryWrapper);
-
-        return R.success(pageInfo);
-    }
-}
+package com.tcl.reggie.controller;import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;import com.baomidou.mybatisplus.extension.plugins.pagination.Page;import com.tcl.reggie.common.BaseContext;import com.tcl.reggie.common.R;import com.tcl.reggie.entity.Dish;import com.tcl.reggie.entity.OrderDetail;import com.tcl.reggie.entity.Orders;import com.tcl.reggie.service.OrderDetailService;import com.tcl.reggie.service.OrdersService;import lombok.extern.slf4j.Slf4j;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.web.bind.annotation.*;import java.util.List;@RestController@RequestMapping("/order")@Slf4jpublic class OrdersController {    @Autowired    private OrdersService orderService;    @Autowired    private OrderDetailService orderDetailService;    @PostMapping("/submit")    public R<String> submit(@RequestBody Orders orders){        orderService.submit(orders);        return R.success("下单成功");    }    /**     * 查询订单详情（手机端），未实现，好像没有页面     * @param page     * @param pageSize     * @return     */    @GetMapping("/userPage")    public R<Page> userPage(int page,int pageSize){        Page<Orders> pageInfo = new Page<>(page, pageSize);        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();        queryWrapper.eq(Orders::getId, BaseContext.getCurrentId());        queryWrapper.orderByDesc(Orders::getOrderTime);        orderService.page(pageInfo, queryWrapper);        return R.success(pageInfo);    }    /**     * 未实现，没有实体类     * @param page     * @param pageSize     * @return     */    @GetMapping("/page")    public R<Page> page(int page, int pageSize){        Page<Orders> pageInfo = new Page<>(page, pageSize);        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();        queryWrapper.eq(Orders::getId, BaseContext.getCurrentId());        queryWrapper.orderByDesc(Orders::getOrderTime);        orderService.page(pageInfo, queryWrapper);        return R.success(pageInfo);    }}
